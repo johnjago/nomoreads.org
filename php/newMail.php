b9f03192d14f5ecc02affac792372ce18f93531e
John Jago <johnjago@protonmail.com>
2018-05-03 21:15:35 -0500

    Remove contact form

--- php/newMail.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if($_SERVER['REQUEST_METHOD'] == "POST") {
-    $headers = 'From: ' . $_POST["firstname"] . ' <' . $_POST["email"] . '>';
-    if (mail("contact@nomoreads.org", $_POST["subject"], $_POST["body"], $headers))
-      echo "We have received your message and will contact you as soon as possible! Thank you!";
-    else
-      echo "An error happened while delivering your message. Please try again later or contact us via Twitter.";
-  }
-?>
